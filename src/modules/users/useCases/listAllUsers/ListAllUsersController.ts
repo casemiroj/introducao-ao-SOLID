0eb27736bfa67820edf38aa27f7223c6a4b72795
João Casemiro <jv.casemiro@icloud.com>
2021-10-01 17:56:05 -0300

    Fix all bugs

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -8,11 +8,14 @@ class ListAllUsersController {
   handle(request: Request, response: Response): Response {
     const { user_id } = request.headers;
 
-    const users = this.listAllUsersUseCase.execute({
-      user_id: String(user_id),
-    });
-
-    return response.status(200).json(users);
+    try {
+      const users = this.listAllUsersUseCase.execute({
+        user_id: String(user_id),
+      });
+      return response.status(200).json(users);
+    } catch (err) {
+      return response.status(400).json({ error: `${err}` });
+    }
   }
 }
 
